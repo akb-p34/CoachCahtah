101dc3a0f79ce023c9eca1484e1e56d1b0f55160
Akbar Pathan <akbar.pathan034@gmail.com>
2023-10-22 05:40:56 -0400

    Used color codes to color map basketball

--- basket.py
+++ basket.py
@@ -1,47 +1,41 @@
 import cv2
 import numpy as np
 
-# Load the video
+# Create a VideoCapture object to read the video
 cap = cv2.VideoCapture('IMG_3627.mov')
 
-while True:
+# Loop until the end of the video
+while(cap.isOpened()):
+    # Capture frame-by-frame
     ret, frame = cap.read()
-    if not ret:
+    
+    if ret == True:
+        # Convert from BGR to HSV color space
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        # Define the range of orange color in HSV
+        lower_orange = np.array([0, 60, 165])
+        upper_orange = np.array([5, 255, 255])
+
+        # Threshold the HSV image to get only orange colors
+        mask = cv2.inRange(hsv_frame, lower_orange, upper_orange)
+
+        # Bitwise-AND mask and original frame
+        res = cv2.bitwise_and(frame, frame, mask=mask)
+
+        # Display the resulting frame
+        cv2.imshow('Frame', frame)
+        cv2.imshow('Mask', mask)
+        cv2.imshow('Result', res)
+
+        # Define 'q' as the exit button
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    else:
         break
 
-    # Convert the frame to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    # Define color range for the ball
-    lower_ball = np.array([hue_lower, sat_lower, val_lower])
-    upper_ball = np.array([hue_upper, sat_upper, val_upper])
-
-    # Create a mask for the ball
-    ball_mask = cv2.inRange(hsv, lower_ball, upper_ball)
-
-    # Find the ball in the mask
-    ball_contours, _ = cv2.findContours(ball_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in ball_contours:
-        # Calculate contour area
-        area = cv2.contourArea(cnt)
-        # Filter out small contours
-        if area > ball_area_threshold:
-            x, y, w, h = cv2.boundingRect(cnt)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    # Use Hough Circle Transform to detect the hoop
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, dp=1.2, minDist=100)
-    if circles is not None:
-        circles = np.round(circles[0, :]).astype("int")
-        for (x, y, r) in circles:
-            cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
-
-    # Display the frame with the ball and hoop highlighted
-    cv2.imshow('Basketball Coaching App', frame)
-
-    if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' to exit
-        break
-
+# Release the video capture object
 cap.release()
+
+# Closes all the windows currently opened.
 cv2.destroyAllWindows()
